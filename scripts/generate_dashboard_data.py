37d8425d302683885b6126ddae1a3bdab89c4dfe
rohansimha <rohans24@uw.edu>
2025-07-28 09:59:47 -0700

    Minor Updates

--- scripts/generate_dashboard_data.py
+++ scripts/generate_dashboard_data.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 def main():
     # Load input files
-    merged = pd.read_csv("./data/merged_users_cuped.csv")
-    uplift = pd.read_csv("./data/uplift_scores.csv")
+    merged = pd.read_csv("../data/merged_users_cuped.csv")
+    uplift = pd.read_csv("../data/uplift_scores.csv")
 
     # Merge by shared features (no ID in uplift)
     merge_cols = ["age", "total_actions", "unique_actions", "total_secs_elapsed", "treatment", "booking"]
@@ -16,7 +16,7 @@ def main():
     final = pd.merge(merged, uplift_subset, on=merge_cols, how="left")
 
     # Output to processed folder
-    final.to_csv("./data/final_dashboard_data.csv", index=False)
+    final.to_csv("../data/final_dashboard_data.csv", index=False)
     print("final_dashboard_data.csv generated successfully.")
 
 if __name__ == "__main__":
